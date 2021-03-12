7d81d2b846a37520421bcabac25cd327d0619559
王贺辉 <wanghehui@bjknrt.com>
2021-03-12 15:39:47 +0800

    生成项目

--- index.js
+++ index.js
@@ -3,16 +3,24 @@
 
 //引入http模块
 const http = require("http");
-//设置主机名
-const hostName = '127.0.0.1';
-//设置端口
-const port = 1011;
-//创建服务
-const server = http.createServer(function(req,res){
-    res.setHeader('Content-Type','text/plain');
-    res.end("hello nodejs");
+// //设置主机名
+// const hostName = '127.0.0.1';
+// //设置端口
+// const port = 1011;
+// //创建服务
+// const server = http.createServer(function(req,res){
+//     res.setHeader('Content-Type','text/plain');
+//     res.end("hello nodejs");
+//
+// });
+// server.listen(port,hostName,function(){
+//     console.log(`服务器运行在http://${hostName}:${port}`);
+// });
 
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Node.js</h1>');
+    res.end('<p>Hello World</p>');
 });
-server.listen(port,hostName,function(){
-    console.log(`服务器运行在http://${hostName}:${port}`);
-});
+server.listen(3000);
+console.log("HTTP server is listening at port 3000.");
